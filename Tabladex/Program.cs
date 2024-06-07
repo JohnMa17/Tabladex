2fb9b4889b015b4b893ffe8b5b12f529d58f1087
ag <ag@DESKTOP-4BN28S3>
2024-06-07 16:38:00 -0400

    Agregar archivos de proyecto.

---
+++ Tabladex/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace tabla
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este es un programa hecho para mostrar la tabla de multiplicar de dicho numero hasta el 12.\n");
+            
+            Console.WriteLine("Ingrese que numero se multiplicara:");
+            int numero = int.Parse(Console.ReadLine());
+            int i = 1;
+
+            while (i <= 12)
+            {
+                Console.WriteLine($"{numero} X {i} = {numero * i} ");
+                i++;
+            }
+        }
+    }
+}
